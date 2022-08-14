4ad5ac172b516fb983ac95371350d21faa5071b2
Jeff Hykin <jeff.hykin@gmail.com>
2022-08-14 10:14:15 -0500

    git subrepo clone https://github.com/jeff-hykin/json_fix.git ./main/blissful_basics/__dependencies__/json_fix
    subrepo:
      subdir:   "main/blissful_basics/__dependencies__/json_fix"
      merged:   "f6eac84"
    upstream:
      origin:   "https://github.com/jeff-hykin/json_fix.git"
      branch:   "master"
      commit:   "f6eac84"
    git-subrepo:
      version:  "0.4.1"
      origin:   "???"
      commit:   "???"

---
+++ main/blissful_basics/__dependencies__/json_fix/main/json_fix/__init__.py
@@ -0,0 +1,58 @@
+import json
+from json import JSONEncoder
+
+# 
+# one-time
+#    monkey-patch the json dumper
+#    this makes external calls to `import json` never know the difference
+#    the object will just auto-serialize
+# 
+
+# check to make sure this only runs once
+if not hasattr(JSONEncoder, "original_default"):
+    from collections import OrderedDict
+    
+    json.override_table = OrderedDict() # this allows for adding serializers to classes you didnt define yourself
+    json.fallback_table = OrderedDict() # this allows for adding generic methods like using str(obj) or obj.__dict__
+    # 
+    # add patch for __json__
+    # 
+    def wrapped_default(self, obj):
+        # 
+        # first check the override_table
+        # 
+        for each_checker in reversed(json.override_table.keys()):
+            type_matches = isinstance(each_checker, type) and isinstance(obj, each_checker)
+            callable_check_matches = not isinstance(each_checker, type) and callable(each_checker) and each_checker(obj)
+            if type_matches or callable_check_matches:
+                custom_converter_function = json.override_table[each_checker]
+                output = custom_converter_function(obj)
+                return output
+        
+        # 
+        # then check the __json__ method
+        # 
+        if hasattr(obj.__class__, "__json__"):
+            json_method = getattr(obj.__class__, "__json__")
+            if callable(json_method):
+                return json_method(obj)
+        
+        # 
+        # then check the fallback_table
+        # 
+        for each_checker in reversed(json.fallback_table.keys()):
+            type_matches = isinstance(each_checker, type) and isinstance(obj, each_checker)
+            callable_check_matches = not isinstance(each_checker, type) and callable(each_checker) and each_checker(obj)
+            if type_matches or callable_check_matches:
+                custom_converter_function = json.fallback_table[each_checker]
+                output = custom_converter_function(obj)
+                return output
+        
+        return wrapped_default.default(obj)
+
+    wrapped_default.default = JSONEncoder().default
+    # apply the patch
+    JSONEncoder.original_default = JSONEncoder.default
+    JSONEncoder.default = wrapped_default
+
+def fix_it(): pass # to support the old interface 
